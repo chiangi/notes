04b8ba0487d982384f0fdd0f7c50db99e354ef22
chiangi <chiangi@users.noreply.github.com>
2017-08-09 17:09:37 -0400

    GUI example using tkinter

---
+++ python/guiapp.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+# empty window
+window = Tk()
+
+def from_kg():
+	grams = float(e1_value.get()) * 1000
+	pounds = float(e1_value.get()) * 2.20462
+	ounces = float(e1_value.get()) * 35.274
+	t1.insert(END, grams)
+	t2.insert(END, pounds)
+	t3.insert(END, ounces)
+	
+l1 = Label(window, text="kg")
+l1.grid(row=0, column=0)
+
+b1 = Button(window, text="Convert", command=from_kg)
+# specify where to place buttom
+#b1.pack()
+# or grid
+b1.grid(row=0, column=2)
+
+e1_value=StringVar()
+e1 = Entry(window, textvariable = e1_value)
+e1.grid(row=0, column=1)
+
+# grams
+t1 = Text(window, height=1, width=20)
+t1.grid(row=1, column=0)
+
+t2 = Text(window, height=1, width=20)
+t2.grid(row=1, column=1)
+
+t3 = Text(window, height=1, width=20)
+t3.grid(row=1, column=2)
+
+window.mainloop()
